0854d67353b407b3c5eca39547d1ebe0b9d85736
Liad Altif <76064879+liadaltif@users.noreply.github.com>
2025-02-26 15:06:02 +0200

    Delete api/terms-of-service.md.js

--- api/terms-of-service.md.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(200).send("tiktok-developers-site-verification=8C0WtIRWMSULhp5eO4SdSXiyTBt3xVzB");
-}
